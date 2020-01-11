5cf656f18f0a0c7210dda79c26c2eb7b9bd3b9c8
Cristian <cheredia@kountable.com>
2020-01-11 15:11:28 -0800

    add tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,2 +1,7 @@
-def test_get_csv_files():
-    assert False
+from helpers import RAW_DATA, TRANSFORMED_DATA, find_csv_files
+
+
+def test_find_csv_files():
+    files = find_csv_files(RAW_DATA)
+    assert isinstance(files, list)
+    assert len(files) >= 1
