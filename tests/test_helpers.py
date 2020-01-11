4564f090d2b31da9ac9c795ff35c6694b6c45229
Cristian <cheredia@kountable.com>
2020-01-11 15:47:07 -0800

    add tests for directory names

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -5,3 +5,7 @@ def test_find_csv_files():
     files = find_files(RAW_DATA)
     assert isinstance(files, list)
     assert len(files) >= 1
+
+def test_directories():
+    assert isinstance(RAW_DATA, str)
+    assert isinstance(PROCESSED_DATA, str)
