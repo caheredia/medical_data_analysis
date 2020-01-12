5e696a355b9506fa9a0d404374af49b659862cb9
Cristian <cheredia@kountable.com>
2020-01-11 16:05:53 -0800

    fix tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,12 +1,21 @@
-from helpers import RAW_DATA, PROCESSED_DATA, find_files
+import subprocess
+from helpers import RAW_DATA, PROCESSED_DATA, find_files, FIGURES
 
 
 def test_find_csv_files():
     files = find_files(RAW_DATA)
     assert isinstance(files, list)
-    assert len(files) >= 1
+    assert files
 
 
 def test_directories():
     assert isinstance(RAW_DATA, str)
     assert isinstance(PROCESSED_DATA, str)
+
+
+def test_make_notebook():
+    subprocess.run(["make", "notebook"])
+    figures = find_files(FIGURES, '.html')
+    assert figures
+    processed_files = find_files(PROCESSED_DATA, '.gzip')
+    assert processed_files
